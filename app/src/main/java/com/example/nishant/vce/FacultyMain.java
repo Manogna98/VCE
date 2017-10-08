c92ce9fc0ac8249d3ff5467ac6f58d99df8df263
Nishant <nmattupalli@gmail.com>
2017-10-09 03:12:44 +0530

    Faculty things done

--- app/src/main/java/com/example/nishant/vce/FacultyMain.java
+++ app/src/main/java/com/example/nishant/vce/FacultyMain.java
@@ -4,17 +4,45 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class FacultyMain extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class FacultyMain extends AppCompatActivity implements View.OnClickListener {
 
    // EditText message_text;
     //public final static String MESSAGE_KEY = "com.example.nishant.vce.message_key";
+
+    private Button l;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty_main);
+
+        l = (Button) findViewById(R.id.button7);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        l.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        logout();
+
+    }
+
+    public void logout(){
+
+        firebaseAuth.signOut();
+        finish();
+
+        Intent intent = new Intent(FacultyMain.this,faculty_login.class);
+        startActivity(intent);
     }
 
     //public void sendMessage(View view){
